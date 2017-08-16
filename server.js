8fd04ad643259c2a5f17230cb1c97cbd80389fa5
Nino Ross Rodriguez <oninross@gmail.com>
2017-08-16 19:46:03 +1000

    - simple chat done

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var app = require('express')(),
-    http = require('http').Server(app);
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function () {
