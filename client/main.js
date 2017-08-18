a86075e5ecb090e1d96720dc602f4f45c9da06b9
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2017-08-18 10:19:31 +1000

    - taking selfies and uploading to database using data URI

--- client/main.js
+++ client/main.js
@@ -12,62 +12,202 @@ firebase.initializeApp(config);
 
 // Get a reference to the database service
 var database = firebase.database(),
-    fbDB = firebase.database().ref(),
+    fbDBref = database.ref(),
     storage = firebase.storage(),
     storageRef = storage.ref(),
-    imgRef;
+    imgRef,
+    newPostRef;
 
 // First we sign in the user anonymously
-firebase.auth().signInAnonymously().then(function () {
-    // first load
-    fbDB.once('value').then(function (snapshot) {
-        var imagesObj = snapshot.val();
+database.ref('/image').on("child_added", function (snapshot) {
+    var v = snapshot.val();
 
-        $.each(imagesObj.image, function (i, v) {
-            imgRef = storageRef.child(v.src);
-
-            // Once the sign in completed, we get the download URL of the image
-            imgRef.getDownloadURL().then(function (url) {
-                // Once we have the download URL, we set it to our img element
-                // document.querySelector('img').src = url;
-                $('.photowall').append($('<img src="' + url + '"/>'));
-            }).catch(function (error) {
-                // If anything goes wrong while getting the download URL, log the error
-                console.error(error);
-            });
-        });
-    });
+    $('.photowall').append($('<img src="' + v.src + '"/>'));
 });
 
 
 
 // Device Camera
-if ($('.camera').length) {
-    Webcam.set({
-        width: $(window).width(),
-        height: $(window).height(),
-        image_format: 'jpeg',
-        jpeg_quality: 90,
-        constraints: {
-            width: { exact: 500 }
-        },
-        flip_horiz: true
+if ($('#camera-stream').length) {
+    var video = document.querySelector('#camera-stream'),
+        image = document.querySelector('#snap'),
+        start_camera = document.querySelector('#start-camera'),
+        controls = document.querySelector('.controls'),
+        take_photo_btn = document.querySelector('#take-photo'),
+        delete_photo_btn = document.querySelector('#delete-photo'),
+        upload_photo_btn = document.querySelector('#upload-photo'),
+        error_message = document.querySelector('#error-message');
+
+
+    // The getUserMedia interface is used for handling camera input.
+    // Some browsers need a prefix so here we're covering all the options
+    navigator.getMedia = (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+
+    if (!navigator.getMedia) {
+        displayErrorMessage("Your browser doesn't have support for the navigator.getUserMedia interface.");
+    }
+    else {
+
+        // Request the camera.
+        navigator.getMedia(
+            {
+                video: true
+            },
+            // Success Callback
+            function (stream) {
+
+                // Create an object URL for the video stream and
+                // set it as src of our HTLM video element.
+                video.src = window.URL.createObjectURL(stream);
+
+                // Play the video element to start the stream.
+                video.play();
+                video.onplay = function () {
+                    showVideo();
+                };
+
+            },
+            // Error Callback
+            function (err) {
+                displayErrorMessage("There was an error with accessing the camera stream: " + err.name, err);
+            }
+        );
+
+    }
+
+
+
+    // Mobile browsers cannot play video without user input,
+    // so here we're using a button to start it manually.
+    start_camera.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        // Start video playback manually.
+        video.play();
+        showVideo();
+
     });
 
-    Webcam.attach('.camera');
 
-    $('.clicker--btn').on('click', take_snapshot);
-}
+    take_photo_btn.addEventListener("click", function (e) {
 
-function take_snapshot() {
+        e.preventDefault();
 
-    // take snapshot and get image data
-    Webcam.snap(function (data_uri) {
-        document.getElementById('photo').innerHTML = '<img src="' + data_uri + '"/>';
-        Webcam.reset();
+        var snap = takeSnapshot();
+
+        // Show image.
+        image.setAttribute('src', snap);
+        image.classList.add("visible");
+
+        // Enable delete and save buttons
+        delete_photo_btn.classList.remove("disabled");
+        upload_photo_btn.classList.remove("disabled");
+
+        // Set the href attribute of the download button to the snap url.
+        upload_photo_btn.href = snap;
+
+        // Pause video playback of stream.
+        video.pause();
+
     });
-}
 
+
+    delete_photo_btn.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        // Hide image.
+        image.setAttribute('src', "");
+        image.classList.remove("visible");
+
+        // Disable delete and save buttons
+        delete_photo_btn.classList.add("disabled");
+        upload_photo_btn.classList.add("disabled");
+
+        // Resume playback of stream.
+        video.play();
+
+    });
+
+    upload_photo_btn.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        newPostRef = fbDBref.child('image');
+
+        console.log(newPostRef)
+
+        newPostRef.push({
+            "src": image.getAttribute('src'),
+            "text": newPostRef.key
+        }).then(function () {
+            alert('upload successful')
+        });
+
+    })
+
+
+
+    function showVideo() {
+        // Display the video stream and the controls.
+
+        hideUI();
+        video.classList.add("visible");
+        controls.classList.add("visible");
+    }
+
+
+    function takeSnapshot() {
+        // Here we're using a trick that involves a hidden canvas element.
+
+        var hidden_canvas = document.querySelector('canvas'),
+            context = hidden_canvas.getContext('2d');
+
+        var width = video.videoWidth,
+            height = video.videoHeight;
+
+        if (width && height) {
+
+            // Setup a canvas with the same dimensions as the video.
+            hidden_canvas.width = width;
+            hidden_canvas.height = height;
+
+            // Make a copy of the current frame in the video on the canvas.
+            context.drawImage(video, 0, 0, width, height);
+
+            // Turn the canvas image into a dataURL that can be used as a src for our photo.
+            return hidden_canvas.toDataURL('image/png');
+        }
+    }
+
+
+    function displayErrorMessage(error_msg, error) {
+        error = error || "";
+        if (error) {
+            console.log(error);
+        }
+
+        error_message.innerText = error_msg;
+
+        hideUI();
+        error_message.classList.add("visible");
+    }
+
+
+    function hideUI() {
+        // Helper function for clearing the app UI.
+
+        controls.classList.remove("visible");
+        start_camera.classList.remove("visible");
+        video.classList.remove("visible");
+        snap.classList.remove("visible");
+        error_message.classList.remove("visible");
+    }
+}
 
 
 
