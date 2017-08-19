1f74c78dc4006c636df0b8d53a8d5dece43c4cf2
Nino Ross Rodriguez <oninross@gmail.com>
2017-08-20 09:34:51 +1000

    - redirect to photo if user is in mobile phone

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var express = require('express'),
-    app = require('express')(),
+    app = express(),
     fs = require('fs'),
     http = require('http').Server(app),
     https = require('https'),
     io = require('socket.io')(http),
+    device = require('express-device'),
+    router = express.Router(),
     // io = require('socket.io')(https),
     httpsOptions = {
         key: fs.readFileSync('key.pem'),
         cert: fs.readFileSync('cert.pem')
     };
 
+app.use(device.capture());
+app.use(router);
 app.use(express.static(__dirname + '/client'));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,11 +25,19 @@ app.use(function (req, res, next) {
     next();
 });
 
+router.get('/', function (req, res) {
+    console.log('\x1b[35m', req.device.type);
+
+    if (req.device.type == 'phone') {
+        res.redirect('/photo');
+    }
+});
+
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    console.log('\x1b[32m', 'a user connected');
 
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+        console.log('\x1b[31m', 'user disconnected');
     });
 
     socket.on('photo flick', function (data) {
@@ -38,5 +50,31 @@ io.on('connection', function (socket) {
 // });
 
 http.listen(process.env.PORT || 8888, function () {
-    console.log('listening on *:8888');
+    console.log('\x1b[36m', 'listening on *:8888');
 });
+
+// Reset = "\x1b[0m"
+// Bright = "\x1b[1m"
+// Dim = "\x1b[2m"
+// Underscore = "\x1b[4m"
+// Blink = "\x1b[5m"
+// Reverse = "\x1b[7m"
+// Hidden = "\x1b[8m"
+
+// FgBlack = "\x1b[30m"
+// FgRed = "\x1b[31m"
+// FgGreen = "\x1b[32m"
+// FgYellow = "\x1b[33m"
+// FgBlue = "\x1b[34m"
+// FgMagenta = "\x1b[35m"
+// FgCyan = "\x1b[36m"
+// FgWhite = "\x1b[37m"
+
+// BgBlack = "\x1b[40m"
+// BgRed = "\x1b[41m"
+// BgGreen = "\x1b[42m"
+// BgYellow = "\x1b[43m"
+// BgBlue = "\x1b[44m"
+// BgMagenta = "\x1b[45m"
+// BgCyan = "\x1b[46m"
+// BgWhite = "\x1b[47m"
