9606a21a29d3580bd4ef69f348c2322928624e43
Nino Ross Rodriguez <oninross@gmail.com>
2017-08-19 14:20:23 +1000

    - added in Masonry to photowall

--- prototype/src/_modules/organisms/photowall/photowall.js
+++ prototype/src/_modules/organisms/photowall/photowall.js
@@ -1,17 +1,31 @@
 'use strict';
 
 import firebase from 'firebase';
+import masonry from 'masonry';
+import imagesloaded from 'imagesloaded';
 
 export default class Photowall {
     constructor() {
         const fbDB = firebase.database(),
-            $photoWall = $('.photowall');
+            $photoWall = $('.photowall ul');
 
         if ($photoWall.length) {
-            fbDB.ref('/image').on("child_added", function (snapshot) {
-                var v = snapshot.val();
+            $photoWall.masonry({
+                itemSelector: ".photowall li",
+                columnWidth: ".photowall__sample"
+            });
 
-                $photoWall.prepend($('<li><img src="' + v.src + '"/></li>'));
+
+            fbDB.ref('/image').on("child_added", function (snapshot) {
+                var v = snapshot.val(),
+                    $items = $('<li><img src="' + v.src + '"/></li>');
+
+                $photoWall.prepend($items).masonry('prepended', $items).imagesLoaded().done(function (instance) {
+                    $photoWall.masonry({
+                        itemSelector: ".photowall li",
+                        columnWidth: ".photowall__sample"
+                    })
+                });
             });
         }
     }
