b28be5f54bba9878ad61d5c3e74cc83bb99bfec2
Nino Ross Rodriguez <oninross@gmail.com>
2017-08-20 12:24:40 +1000

    - updated service worker - updated bump task - clean up

--- prototype/src/service-worker.js
+++ prototype/src/service-worker.js
@@ -1,28 +1,44 @@
-const OFFLINE_URL = '/pages/offline/';
+/*
+ Copyright 2014 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
-importScripts('sw-toolbox.js');
+// While overkill for this specific sample in which there is only one cache,
+// this is one best practice that can be followed in general to keep track of
+// multiple caches used by a given service worker, and keep them all versioned.
+// It maps a shorthand identifier for a cache to a specific, versioned cache name.
 
-const urlsToPrefetch = [
-    'assets/interactive-wall/css/main.css',
-    'assets/interactive-wall/js/main.js',
-    'assets/interactive-wall/css/fonts/icomoon.woff',
-    'manifest.json',
-    OFFLINE_URL,
-    'index.html',
-    '/'
-];
+// Note that since global state is discarded in between service worker restarts, these
+// variables will be reinitialized each time the service worker handles an event, and you
+// should not attempt to change their values inside an event handler. (Treat them as constants.)
 
-self.toolbox.router.default = self.toolbox.networkFirst;
-
-self.toolbox.router.get('/(.*)', function (req, vals, opts) {
-    return self.toolbox.networkFirst(req, vals, opts)
-        .catch(function (error) {
-            if (req.method === 'GET' && req.headers.get('accept').includes('text/html')) {
-                return self.toolbox.cacheOnly(new Request(OFFLINE_URL), vals, opts);
-            }
-            throw error;
-        });
-});
+// If at any point you want to force pages that use this service worker to start using a fresh
+// cache, then increment the CACHE_VERSION value. It will kick off the service worker update
+// flow and the old cache(s) will be purged as part of the activate event handler when the
+// updated service worker is activated.
+var version = '0.0.3',
+    now = Date.now(),
+    OFFLINE_URL = 'offline/',
+    urlsToPrefetch = [
+        'assets/interactive-wall/css/main.css',
+        'assets/interactive-wall/js/main.js',
+        'assets/interactive-wall/css/fonts/icomoon.woff',
+        'manifest.json',
+        OFFLINE_URL,
+        'index.html',
+        '/'
+    ],
+    CURRENT_CACHES = {
+        prefetch: 'prefetch-cache-v' + version
+    };;
 
 self.addEventListener('install', function (event) {
     // All of these logging statements should be visible via the "Inspect" interface
@@ -101,6 +117,39 @@ self.addEventListener('activate', function (event) {
                     }
                 })
             );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+        // caches.match() will look for a cache entry in all of the caches available to the service worker.
+        // It's an alternative to first opening a specific named cache and then matching on self.
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                console.log('Found response in cache:', response);
+
+                return response;
+            }
+
+            console.log('No response found in cache. About to fetch from network...');
+
+            // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
+            // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
+            return fetch(event.request).then(function (response) {
+                console.log('Response from network is:', response);
+
+                return response;
+            }).catch(function (error) {
+                // This catch() will handle exceptions thrown from the fetch() operation.
+                // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+                // It will return a normal response object that has the appropriate error code set.
+                console.error('Fetching failed:', error);
+
+                return caches.match(OFFLINE_URL);
+            });
         })
     );
 });
